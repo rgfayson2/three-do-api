fc51d0e0e4a4801d1367bb634e7b0bc450b52b13
Roderick Fayson <rgfayson2@gmail.com>
2022-08-26 11:02:38 -0400

    fixed error

--- functions/dbConnect.js
+++ functions/dbConnect.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase-admin";
 import { cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore"
-import { credentials } from "./credentials.js";
+import { credentials } from "./src/credentials.js";
 
 
 export default function dbConnect() {
